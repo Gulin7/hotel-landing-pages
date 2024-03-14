9a63977481def6f2d572ce45e2c4b0f7153d5fe4
Tudor Gulin <gulintudor@gmail.com>
2024-03-15 01:19:34 +0200

    hotel-layout-3

---
+++ hotel-layout-3/js/main.js
@@ -0,0 +1,27 @@
+//Smooth Scrolling Brad Traversy
+$('.page .btn').on('click', function (e) {
+	if (this.hash !== '') {
+		e.preventDefault()
+
+		const hash = this.hash
+		$('html, body').animate(
+			{
+				scrollTop: $(hash).offset().top,
+			},
+			800
+		)
+	}
+})
+// Horizontal Scroll
+
+let rightArrow1 = document.querySelector('#about-1 .right-arrow')
+let leftArrow1 = document.querySelector('#about-1 .left-arrow')
+
+rightArrow1.addEventListener('click', function () {
+	let scroll = document.querySelector('#about-1 .panel')
+	scroll.scrollLeft += 200
+})
+leftArrow1.addEventListener('click', function () {
+	let scroll = document.querySelector('#about-1 .panel')
+	scroll.scrollLeft -= 200
+})
